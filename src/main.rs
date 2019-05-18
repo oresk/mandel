27e70dbc1d3063296cb00c7078fcd701eed7d2b6
Lovro Oreskovic <lovro@oreskovic.me>
2019-05-18 19:50:45 +0200

    switched from ndarray to std array

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,44 @@
 // https://hardmath123.github.io/scratch-mandelbrot.html
 
 // include png output
-// include complex numbers
+use std::env;
+use std::convert::TryFrom;
 use num::complex::Complex;
-use ndarray::Array2;
 
 fn main() {
     println!("Welcome to mandelbrot set display!");
-    let zoom = 100;
-    let zero = Complex{re: 10.0, im: 10.0};
-    let size = Complex{re: 50, im: 50};
-    let boundary = 50;
+    let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
 
-let mut image = Array2::<u32>::zeros((size.re, size.im));
+    let mut image = vec![vec![0; usize::try_from(config.size.re).unwrap()]; usize::try_from(config.size.im).unwrap()];
 
-for i in 0..image.rows() {
-    for j in 0..image.cols(){
-        image[[i,j]] = calculate_mandelbrot_pixel(Complex{re: i as f64 , im: j as f64} - zero, boundary);
-        println!("item {}{} value: {}", i, j, image[[i,j]]);
+    for i in 0..config.size.re {
+        for j in 0..config.size.im{
+            image[i as usize][j as usize] = calculate_mandelbrot_pixel((Complex{re: i as f64 , im: j as f64} - config.zero) / config.zoom, config.boundary);
+            print!("{} ",image[i as usize][j as usize]);
+            //print!("|{} {}|",i,j);
+        }
+        println!("");
     }
-}
 
 // create png from image array
 
+}
+
+struct Config{
+    zoom: Complex<f64>,
+    zero: Complex<f64>,
+    size: Complex<u32>,
+    boundary: u32,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let zoom = Complex{re: args[1].clone().parse().unwrap(), im: args[2].clone().parse().unwrap()};
+    let zero = Complex{re: args[3].clone().parse().unwrap(), im: args[4].clone().parse().unwrap()};
+    let size = Complex{re: args[5].clone().parse().unwrap(), im: args[6].clone().parse().unwrap()};
+    let boundary = args[7].clone().parse::<u32>().unwrap();
+
+    Config { zoom, zero, size, boundary }
 }
 
 fn calculate_mandelbrot_pixel(location: Complex<f64>, boundary: u32 ) -> u32{
