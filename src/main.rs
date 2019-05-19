79b8f51d770d2c8ee69cb431a193f97578c67a70
Lovro Oreskovic <lovro@oreskovic.me>
2019-05-19 20:29:21 +0200

    add input arguments by using clap cargo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // https://hardmath123.github.io/scratch-mandelbrot.html
-
+#[macro_use]
+extern crate clap;
 // include png output
 use std::env;
 use std::convert::TryFrom;
@@ -18,8 +19,8 @@ fn main() {
         for j in 0..image[i].len() {
             image[i][j] = calculate_mandelbrot_pixel((Complex{re: i as f64 , im: j as f64} - config.zero) / config.zoom, config.boundary);
             print!("{:3} ",image[i][j]);
-            //print!("|{} {}|",i,j);
         }
+        println!("");
         println!("");
     }
 
@@ -35,10 +36,38 @@ struct Config{
 }
 
 fn parse_config(args: &[String]) -> Config {
-    let zoom = Complex{re: args[1].clone().parse().unwrap(), im: args[2].clone().parse().unwrap()};
-    let zero = Complex{re: args[3].clone().parse().unwrap(), im: args[4].clone().parse().unwrap()};
-    let size = Complex{re: args[5].clone().parse().unwrap(), im: args[6].clone().parse().unwrap()};
-    let boundary = args[7].clone().parse::<u32>().unwrap();
+    let matches = App::new("Mandelbrot generator")
+                          .version("0.1.0")
+                          .author("Lovro Oreskovic <lovro@oreskovic.me>")
+                          .about("Generates ascii representation of mandelbrot set")
+                          .arg(Arg::with_name("ZEROx")
+                               .help("Sets the x zero location.")
+                               .required(true)
+                               .index(1))
+                          .arg(Arg::with_name("ZEROy")
+                               .help("Sets the y zero location.")
+                               .required(true)
+                               .index(2))
+                          .arg(Arg::with_name("SIZEx")
+                               .help("Sets the x size of the output.")
+                               .required(true)
+                               .index(3))
+                          .arg(Arg::with_name("SIZEy")
+                               .help("Sets the y size of the output.")
+                               .required(true)
+                               .index(4))
+                          .arg(Arg::with_name("ZOOM")
+                               .help("Sets the zoom of the images. By default set to 10.")
+                               .index(5))
+                          .arg(Arg::with_name("BOUNDARY")
+                               .help("Sets the boundary of the calculation. By default set to 50.")
+                               .index(6))
+                          .get_matches();
+
+    let zoom = Complex{re: value_t!(matches, "ZOOM", f64).unwrap_or(10.0), im: 0.0};
+    let zero = Complex{re: value_t!(matches, "ZEROx", f64).unwrap(), im: value_t!(matches, "ZEROy", f64).unwrap() };
+    let size = Complex{re: value_t!(matches, "SIZEx", u32).unwrap(), im: value_t!(matches, "SIZEy", u32).unwrap() };
+    let boundary = value_t!(matches, "BOUNDARY", u32).unwrap_or(50);
 
     Config { zoom, zero, size, boundary }
 }
